3e47285f64b47e0baf23e3d80b217605095d00db
Tyler Robbins <tyler@familyrobbins.com>
2014-09-11 11:33:32 -0700

    Added new imports. (SpellCard) Added new initialization parameters. Added new utility methods to get attributes indirectly. Added comments to every method. Added drops as an attribute. [Card] Added default function to do. [dispName] Added wrap-line to the name so that it doesn't overlap with the timer. [ChangeBKG] 'blits' the image itself instead of returning it. [idle] Added default function to do. [stopCard] Only stops the card if it's running. This prevents log file spam. [dispTime] Added color parameter. [renderInfo] Properly displays time and name without overlap. (LargeEX) Added new attributes. [Card] Rebuilt spellcard from scratch. [idle] Rebuilt idle. [getBGroup] Moved to parent class.

--- bin/lib/spellcards.py
+++ bin/lib/spellcards.py
@@ -3,24 +3,51 @@
 #Spellcards
 #Define all usable spell cards
 
+import pygame
+
 from basic import *
-# from sprites import *
+from bullets import *
+from timerv2 import *
+from fontlib import *
 from globalVar import *
 from constants import *
-from timerv2 import *
+
+# import fontlib,
 
 class SpellCard(object):
-	def __init__(self,time,name,owner,ownerGroup):
-		self.start = 	False
-		self.name = 	name
+	def __init__(self,time,name,owner,ownerGroup,playerb=False,newMaxLife=1000):
+		'''A generic spellcard class that will spawn bullets in a pre-defined pattern
+		   time 		<- How long is the spellcard supposed to last in milliseconds (i.e: 30 seconds = 30000 milliseconds)
+		   owner 		<- The owner of the spellcard. (i.e: boss, player, fairy1, etc.).
+		   ownerGroup 	<- The bullet group of the owner (i.e: bossBullet, playerBullet, etc.)
+		   name="" 		<- The name of the spellcard. This is optional, and will default to empty string.
+		'''
+		self.start = 	False #Does not start until runCard is called
+		self.name = 	name  #Name of spellcard
 		self.time = 	time
+		self.namepos = 	None
+		self.timepos = 	(OVERLAX/2-20,5)
 
 		self.timer = 	Timer(time)
 
 		self.owner = 		owner
 		self.ownerGroup = 	ownerGroup
 
+		self.playerb = playerb
+
+		self.spellBKG = pygame.Surface(overlay.get_size())
+		self.spellBKG.fill(WHITE)
+
+		self.last_time = 0
+
+		self.owner.speed = [3,0]
+
+		self.newMax = newMaxLife
+
+		self.drops = {"p":0}
+
 	def runCard(self):
+		'''This will begin the spellcard when called for the first time. It must be continuously called by idle.'''
 		if not self.start:
 			self.start = True
 			self.timer.startTimer()
@@ -30,83 +57,151 @@ class SpellCard(object):
 		if self.timer.isFinished():
 			self.owner.kill()
 			self.stopCard()
-		# self.timer.timer() # <- This cannot be run with the new timer
 
 	def Card(self):
-		'''This will spawn bullets when SpellCard is called'''
-		pass
+		'''This defines what bullets to spawn and when, and must be overwritten by custom spellcards.'''
+		if self.start:
+			# self.ctime = (self.timer.getTimePassed()/1000) - self.last_time
+			self.ctime = pygame.time.get_ticks()/1000-self.last_time
+
+			if self.ctime >= 1:
+				self.last_time = pygame.time.get_ticks()/1000
+
+				for i in range(4):
+					start = surf_center(self.owner.image,BULL2)
+					start[0] += self.owner.rect.x
+					start[1] += self.owner.rect.y
+
+					if i is 0:
+						b = circleShot(start,(0,-5))
+					elif i is 1:
+						b = circleShot(start,(0,5))
+					elif i is 2:
+						b = circleShot(start,(-5,0))
+					elif i is 3:
+						b = circleShot(start,(5,0))
+
+					self.ownerGroup.add(b)
 
 	def isStart(self):
+		'''Return if the spellcard has been started.'''
 		return self.start
 
 	def getName(self):
+		'''Return spellcard name as a string.'''
 		return self.name
 
 	def dispName(self,pos,color=BLACK):
-		namee = fontObj.render(self.name,True,color)
-		overlay.blit(namee,pos)
+		'''Display the spellcard name on the overlay.'''
+		name = 		wrapline(self.name,fontObj,overlay,pos)
+		ystart = 	pos[1]
 
-	def ChangeBKG(self,img):
-		# '''Don't know how to successfully use this yet, so I'm leaving it blank for now'''
-		# overlay.blit(img,(0,0))
-		return img
+		for n in name:
+			namee = fontObj.render(n,True,color)
+
+			overlay.blit(namee,(pos[0],ystart))
+			ystart += namee.get_height()
 
 	def idle(self):
-		pass
+		'''Default idle method. Can be overwritten for custom content.'''
+		if self.start:
+			self.runCard() #Run spellcard
+
+			self.renderInfo(self.timepos,overlay,fontObj) #Render name and time
+
+		return self.spellBKG #
 
 	def stopCard(self):
-		self.start = False
-		# self.timer.pauseTimer()
-		self.timer.reset()
+		'''End the spellcard if it hasn't already'''
+		if self.start:
+			self.start = False
+			self.timer.forceStop()
+			self.timer.reset()
 
 	def changeOwnerPos(self,speed,newPos):
+		'''Change the position of the owner. Will move the owner incrementally if they are not already there.'''
 		if self.owner.rect.x < newPos[0]: self.owner.rect.x += speed
 		if self.owner.rect.x > newPos[0]: self.owner.rect.x -= speed
 		if self.owner.rect.y < newPos[1]: self.owner.rect.y += speed
 		if self.owner.rect.y > newPos[1]: self.owner.rect.y -= speed
 
-	def dispTime(self,pos,surf,font,cutoff=0,count=0):
-		self.timer.dispTime(pos,surf,font,cutoff=cutoff)
+	def dispTime(self,pos,surf,font,color=BLACK,cutoff=0,count=0):
+		'''Display current time.'''
+		self.timer.dispTime(pos,surf,font,color=color,cutoff=cutoff,count=count)
+
+	def renderInfo(self,pos,surf,font,color1=BLACK,color2=BLACK):
+		self.dispTime(pos,surf,font,color=color1,cutoff=1,count=0)
+
+		fsize = self.timer.getTimeFont(font).get_size()
+
+		pos2 = (pos[0]+fsize[0],pos[1])
+
+		if not self.namepos:
+			self.namepos = pos2
+
+		self.dispName(self.namepos,color2)
+
+	def getBGroup(self):
+		'''Returns bullet group'''
+		return self.ownerGroup
+
+	def getDrops(self):
+		'''Returns all items to drop'''
+		return self.drops
+
+	def getNewLife(self):
+		'''Give the owner a new maximum life.'''
+		self.owner.setMaxLife(self.newMax)
+
+	def getSpellBKG(self):
+		'''Get spellcard background.'''
+		return self.spellBKG
 
 class LargeEX(SpellCard):
 	def __init__(self,owner,ownerGroup,playerb=False):
-		SpellCard.__init__(self,30000,"Large X: Generic Danmaku",owner,ownerGroup)
+		SpellCard.__init__(self,30000,"Large X: Generic Danmaku",owner,ownerGroup,playerb=playerb,newMaxLife=1500)
 
-		self.spawn2 = [0,0]
-		self.spawn3 = [OVERLAX-40,0]
-		# self.ctime = float(pygame.time.get_ticks())/1000 - float(self.last_time)/1000
-		self.ctime = self.timer.getTimePassed()
+		self.spawn2 =		[0,0]
+		self.spawn3 =		[OVERLAX-40,0]
+		self.timepos = 		(OVERLAX/2-20,5)
 
+		self.ctime = 		float(pygame.time.get_ticks())/1000
+		self.last_time = 	self.ctime
+		# self.ctime = self.timer.getTimePassed()
+
+		# del self.spellBKG
 		self.spellBKG = loadImage("SpellBKG.png",fail_size=OVERSIZE)
 
-		self.playerb = playerb
+		self.drops = {'p':0,'p':1,'s':0,'p':0}
 
 	def Card(self):
-		# self.shoot(5,group,surf)
 		if self.start:
+			newPos = [surf_center(overlay,self.owner.image)[0],10]
+			
+			self.changeOwnerPos(5,newPos)
+			self.owner.speed = [0,0]
+
 			name_size = fontObj.render(self.name,True,BLACK).get_width()
 			pos = (OVERLAX-(name_size+5),10)
-			self.dispName(pos)
-			# self.ctime = float(pygame.time.get_ticks())/1000 - float(self.last_time)/1000
-			self.ctime = self.timer.getTimePassed()
+			# self.dispName(pos)
 
-			if self.ctime >= 0.3:
-				b2 = circleShot(spawn2,(5,5),self.playerb)
-				b = circleShot(spawn3,(-5,5),self.playerb)
+			self.ctime = float(pygame.time.get_ticks())/1000
 
-				self.last_time = pygame.time.get_ticks()
+			if self.ctime - self.last_time >= 0.3:
+				b2 = circleShot(self.spawn2,(5,5),self.playerb)
+				b = circleShot(self.spawn3,(-5,5),self.playerb)
+
+				self.last_time = self.ctime
 
 				self.ownerGroup.add(b)
 				self.ownerGroup.add(b2)
 
 	def idle(self):
 		self.runCard()
-		# self.Card()
-		# self.timer.timer()
+		# self.dispTime((OVERLAX/2-20,5),overlay,fontObj,cutoff=1)
+		
+		self.renderInfo(self.timepos,overlay,fontObj)
 
-		return self.ChangeBKG(self.spellBKG)
-
-	def getBGroup(self):
-		return self.ownerGroup
+		return self.spellBKG
 
 nuclear = u'\u2622'

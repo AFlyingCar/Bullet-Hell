701d8f88ad5be4227f085dd2d1250c4be381f61b
Tyler Robbins <tyler@familyrobbins.com>
2014-10-26 14:57:20 -0700

    Removed the file because it is unused.

--- bin/lib/enemies.py
+++ bin/lib/enemies.py
@@ -4,16 +4,15 @@
 # Define the classes for all enemies.
 
 from sprites import *
-from spellcards import *
+# from .sprites import Fairy
 from dependencies import *
 
-class VivianJames(boss):
-	def __init__(self,num,life,lives):
-		boss.__init__(self,num,BOSS11_IMG,"th00_02.ogg",life=life,lives=lives)
-		self.speed = [0,0]
+class Fairy(object):
+	def __init__(self,num,img,life):
+		pass
 
-		spell1 = LargeEX(self,self.bulletGroup)
-
-		self.spells.append(spell1)
+class Fairy1(Fairy):
+	def __init__(self,pos,life=4):
+		Fairy.__init__(self,pos,FAIRY1_IMG,life=life)
 
 nuclear = u'\u2622'
